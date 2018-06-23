e5b954942f44c6d25dd4cf122c747ca5a159e7d3
Douglas Bacon <dugbdev@gmail.com>
2018-06-23 15:02:24 -0400

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -9,7 +9,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
-        self.browser.close()
+        self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').header_text
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
